cef6bc47e61239e17666525905c1477c1b45af4d
wwren <rdding@student.unimelb.edu.au>
2021-06-21 20:49:06 +1000

    week 3 controlled form part1

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,5 +1,15 @@
 import React from "react";
-import { Card, CardImg, CardText, CardBody, CardTitle, List } from "reactstrap";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  List,
+  Breadcrumb,
+  BreadcrumbItem,
+} from "reactstrap";
+import { Link } from "react-router-dom";
 
 function RenderDish({ selectedDish }) {
   return (
@@ -42,11 +52,26 @@ const DishDetail = (props) => {
     return (
       <div className="container">
         <div className="row">
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link to="/home">Home</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem>
+              <Link to="/menu"> Menu</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>{props.selectedDish.name}</BreadcrumbItem>
+          </Breadcrumb>
+          <div className="col-12">
+            <h3>{props.selectedDish.name}</h3>
+            <hr />
+          </div>
+        </div>
+        <div className="row">
           <div className="col-12 col-md-5 m-1">
             <RenderDish selectedDish={props.selectedDish} />
           </div>
           <div className="col-12 col-md-5 m-1">
-            <RenderComments comments={props.selectedDish.comments} />
+            <RenderComments comments={props.comments} />
           </div>
         </div>
       </div>
