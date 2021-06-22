847e93cb5d16970077441e528e96e29416aff1bf
wwren <rdding@student.unimelb.edu.au>
2021-06-22 22:04:46 +1000

    controlled forms

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -8,6 +8,7 @@ import {
   Label,
   Input,
   Col,
+  FormFeedback
 } from "reactstrap";
 import { Link } from "react-router-dom";
 
@@ -23,17 +24,26 @@ class Contact extends Component {
       agree: false,
       contactType: "Tel.",
       message: "",
+      touched: {
+        firstname: false,
+        lastname: false,
+        telnum: false,
+        email: false
+      }
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
+    // this.validate = this.validate.bind(this);
   }
 
   handleInputChange(event) {
     const target = event.target;
     const value = target.type === "checkbox" ? target.checked : target.value;
     const name = target.name;
-
+    //     the syntax [] is to set a key of an object without knowing ahead of time what you want it to be called
+    // - a computed property name
     this.setState({
       [name]: value,
     });
@@ -45,7 +55,59 @@ class Contact extends Component {
     event.preventDefault();
   }
 
+  handleBlur = (field )  => (evt)=> {
+    this.setState({
+      touched: {...this.state.touched, [field]: true}
+    })
+  }
+  // handleBlur = (event) => {
+  //   this.setState({
+  //     touched: {...this.state.touched,
+  //     [event.target.name]: true
+  //     }
+  //   })
+  // }
+  validate(firstname, lastname, telnum, email) {
+    const errors = {
+      firstname: "",
+      lastname: "",
+      telnum: "",
+      email: ""
+    }
+
+    if (this.state.touched.firstname && firstname.length < 3)
+    {
+      errors.firstname = 'First name shoud be >= 3 characters'
+    } else if (this.state.touched.firstname && firstname.length >10)
+    {
+      errors.firstname = 'First name shoud be <= 10 characters'
+    }
+
+    if (this.state.touched.lastname && lastname.length < 3)
+    {
+      errors.lastname = 'Last name shoud be >= 3 characters'
+    } else if (this.state.touched.lastname && lastname.length >10)
+    {
+      errors.lastname = 'Last name shoud be <= 10 characters'
+    }
+
+    const reg = /^\d+$/;
+    if (this.state.touched.telnum && !reg.test())
+    {
+      errors.telnum = 'Tel. Number shoud contain only numbers'
+    }
+
+    if (this.state.touched.email && email.split('').filter(x => x === '@').length !== 1)
+    {
+      errors.email = 'Email shoud contain @'
+    }
+
+    return errors;
+  }
+
   render() {
+    console.log('form updated')
+    let errors = this.validate(this.state.firstname, this.state.lastname, this.state.telnum, this.state.email)
     return (
       <div className="container">
         <div className="row">
@@ -104,6 +166,139 @@ class Contact extends Component {
                 <i className="fa fa-envelope-o"></i> Email
               </a>
             </div>
+          </div>
+        </div>
+        <div className="row row-content">
+          <div className="col-12">
+            <h3>Send us your Feedback</h3>
+          </div>
+          <div className="col-12 col-md-9">
+            <Form onSubmit={this.handleSubmit}>
+              <FormGroup row>
+                <Label htmlFor="firstname" md={2}>
+                  First Name
+                </Label>
+                <Col md={10}>
+                  <Input
+                    type="text"
+                    id="firstname"
+                    name="firstname"
+                    valid={errors.firstname === ''}
+                    invalid={errors.firstname !== ''}
+                    placeholder="First Name"
+                    value={this.state.firstname}
+                    onBlur = {this.handleBlur('firstname')}
+                    onChange={this.handleInputChange}
+                  />
+                  <FormFeedback>{errors.firstname}</FormFeedback>
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label htmlFor="lastname" md={2}>
+                  Last Name
+                </Label>
+                <Col md={10}>
+                  <Input
+                    type="text"
+                    id="lastname"
+                    name="lastname"
+                    placeholder="Last Name"
+                    value={this.state.lastname}
+                    onBlur = {this.handleBlur('lastname')}
+                    onChange={this.handleInputChange}
+                    valid={errors.lastname === ''}
+                    invalid={errors.lastname !== ''}
+                  />
+                  <FormFeedback>{errors.lastname}</FormFeedback>
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label htmlFor="telnum" md={2}>
+                  Contact Tel.
+                </Label>
+                <Col md={10}>
+                  <Input
+                    type="tel"
+                    id="telnum"
+                    name="telnum"
+                    placeholder="Tel. number"
+                    value={this.state.telnum}
+                    onChange={this.handleInputChange}
+                    onBlur = {this.handleBlur('telnum')}
+                    valid={errors.telnum === ''}
+                    invalid={errors.telnum !== ''}
+                  />
+                     <FormFeedback>{errors.telnum}</FormFeedback>
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label htmlFor="email" md={2}>
+                  Email
+                </Label>
+                <Col md={10}>
+                  <Input
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="Email"
+                    value={this.state.email}
+                    onChange={this.handleInputChange}
+                    onBlur = {this.handleBlur('email')}
+                    valid={errors.email === ''}
+                    invalid={errors.email !== ''}
+                  />
+                     <FormFeedback>{errors.email}</FormFeedback>
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Col md={{ size: 6, offset: 2 }}>
+                  <FormGroup check>
+                    <Label check>
+                      <Input
+                        type="checkbox"
+                        name="agree"
+                        checked={this.state.agree}
+                        onChange={this.handleInputChange}
+                      />
+                      <strong>May we contact you?</strong>
+                    </Label>
+                  </FormGroup>
+                </Col>
+                <Col md={{ size: 3, offset: 1 }}>
+                  <Input
+                    type="select"
+                    name="contactType"
+                    value={this.state.contactType}
+                    onChange={this.handleInputChange}
+                  >
+                    <option>Tel.</option>
+                    <option>Email</option>
+                  </Input>
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label htmlFor="message" md={2}>
+                  Your Feedback
+                </Label>
+                <Col md={10}>
+                  <Input
+                    type="textarea"
+                    id="message"
+                    name="message"
+                    rows="12"
+                    value={this.state.message}
+                    onChange={this.handleInputChange}
+                  ></Input>
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Col md={{ size: 10, offset: 2 }}>
+                  <Button type="submit" color="primary">
+                    Send Feedback
+                  </Button>
+                </Col>
+              </FormGroup>
+            </Form>
           </div>
         </div>
       </div>
