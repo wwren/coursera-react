4df5ea95f1a48e94615364399a23f9b20feb87d9
wwren <rdding@student.unimelb.edu.au>
2021-06-26 14:50:21 +1000

    intro to redux

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,38 +3,34 @@ import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Menu from "./MenuComponent";
 import Home from "./HomeComponent";
-import { Switch, Route, Redirect } from "react-router-dom";
-import { DISHES } from "../shared/dishes";
-import { COMMENTS } from "../shared/comments";
-import { LEADERS } from "../shared/leaders";
-import { PROMOTIONS } from "../shared/promotions";
+import { Switch, Route, Redirect, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
 import DishDetail from "./DishDetailComponent.js";
 import Contact from "./ContactComponent";
 import About from "./AboutComponent";
 
+const mapStateToProps = (state) => {
+  return {
+    dishes: state.dishes,
+    comments: state.comment,
+    promotions: state.promotions,
+    leaders: state.leaders,
+  };
+};
+
+// previously main holds and maintains the states, now need to get the states from store
 class Main extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      dishes: DISHES,
-      comments: COMMENTS,
-      promotions: PROMOTIONS,
-      leaders: LEADERS,
-      // selectedDish: null,
-    };
   }
-
-  // onDishSelect(dishId) {
-  //   this.setState({ selectedDish: dishId });
-  // }
 
   render() {
     const HomePage = () => {
       return (
         <Home
-          dish={this.state.dishes.find((dish) => dish.featured)}
-          promotion={this.state.promotions.find((ele) => ele.featured)}
-          leader={this.state.leaders.find((leader) => leader.featured)}
+          dish={this.props.dishes.find((dish) => dish.featured)}
+          promotion={this.props.promotions.find((ele) => ele.featured)}
+          leader={this.props.leaders.find((leader) => leader.featured)}
         />
       );
     };
@@ -42,10 +38,10 @@ class Main extends Component {
     const DishWithId = ({ match }) => {
       return (
         <DishDetail
-          selectedDish={this.state.dishes.find(
+          selectedDish={this.props.dishes.find(
             (dish) => dish.id == parseInt(match.params.dishId, 10)
           )}
-          comments={this.state.comments.filter(
+          comments={this.props.comments.filter(
             (comment) => comment.dishId == parseInt(match.params.dishId, 10)
           )}
         ></DishDetail>
@@ -59,12 +55,12 @@ class Main extends Component {
           <Route path="/home" component={HomePage}></Route>
           <Route
             path="/aboutus"
-            component={() => <About leaders={this.state.leaders} />}
+            component={() => <About leaders={this.props.leaders} />}
           ></Route>
           <Route
             exact
             path="/menu"
-            component={() => <Menu dishes={this.state.dishes} />}
+            component={() => <Menu dishes={this.props.dishes} />}
           ></Route>
           <Route path="/menu/:dishId" component={DishWithId}></Route>
           <Route exact path="/contactus" component={Contact}></Route>
@@ -75,5 +71,5 @@ class Main extends Component {
     );
   }
 }
-
-export default Main;
+// connect react components to react router
+export default withRouter(connect(mapStateToProps)(Main));
